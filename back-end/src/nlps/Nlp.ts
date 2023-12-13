912cc833513f1d3dcc84e6bd1720a11495da594b
Kulshresth Jangid <kulshresthjangid@gmail.com>
2023-12-14 01:28:54 +0530

    removed comments && added entities addition in the Nlp class && formatting code

--- back-end/src/nlps/Nlp.ts
+++ back-end/src/nlps/Nlp.ts
@@ -25,7 +25,6 @@ class Nlp {
     protected languages: string[];
 
     constructor(languages: string[]) {
-        // Assuming NlpManagerOptions here, adjust as needed based on library updates
         const nlpManagerOptions: NlpManagerSettings = {
             languages: languages,
         };
@@ -33,23 +32,17 @@ class Nlp {
         this.languages = languages;
     }
 
-    async createIntent(intentName: string, utterance: string, entities?: string[]): Promise<void> {
-        if (entities) {
-            // Handle entities if needed
-            // For example, you can loop through the entities and add them to the intent
+    async createIntent(intentName: string, utterance: string, entities?: string[], categoryName?: string): Promise<void> {
+        if (entities && categoryName) {
+            await entities.forEach(el => this.createAndAddEntity(categoryName, el, [utterance]));;
         }
-
-        // Adding document to the NLP manager
         await this.nlpManager.addDocument(this.languages[0], utterance, intentName);
-        // Train the model after adding a document
         await this.nlpManager.train();
     }
 
     async createAndAddEntity(categoryName: string, entityName: string, utterances: string[]): Promise<void> {
-        // Add entity to the NLP manager
         this.nlpManager.addNamedEntityText(categoryName, entityName, this.languages || [], utterances);
 
-        // Train the model after adding entities
         await this.nlpManager.train();
     }
 }
